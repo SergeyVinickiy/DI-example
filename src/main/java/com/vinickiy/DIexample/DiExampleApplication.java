ed4edcb6c5a078db4f8a737c9e65edc707b34dca
SergeyV1 <sergey.vinickiy@verifone.com>
2020-03-28 19:41:02 +0300

    Initial Project Add

---
+++ src/main/java/com/vinickiy/DIexample/DiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.vinickiy.DIexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiExampleApplication.class, args);
+	}
+
+}
